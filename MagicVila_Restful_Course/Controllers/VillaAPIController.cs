de5ba58957d51a2641ff16139e9f70aa45bb3b51
Rai Paramartha <araipars@gmail.com>
2023-04-26 22:27:06 +0700

    add logger and status code

---
+++ MagicVila_Restful_Course/Controllers/VillaAPIController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicVila_Restful_Course.Data;
+using MagicVila_Restful_Course.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagicVila_Restful_Course.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        private readonly ILogger<VillaAPIController> _logger;
+
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult<IEnumerable<Villa>> GetVillas()
+        {
+            _logger.LogInformation("Getting all villas");
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200, Type =typeof(VillaDTO))]
+        public ActionResult<Villa> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                _logger.LogError("Get Villa Error with Id " + id);
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<Villa> CreateVilla([FromBody] Villa villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest(villa);
+            }
+            if (villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villa);
+
+
+            return CreatedAtRoute("GetVilla", new { id = villa.Id }, villa);
+        }
+    }
+}
+
---
+++ MagicVila_Restful_Course/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MagicVila_Restful_Course.Models;
+namespace MagicVila_Restful_Course.Data
+{
+    public static class VillaStore
+    {
+        public static List<Villa> villaList = new List<Villa> {
+            new Villa{Id=1,Name="Pool View" },
+            new Villa{Id=2,Name="Beach View" }
+            };
+    }
+}
+
